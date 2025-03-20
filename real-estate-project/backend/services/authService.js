bcbb2010428bfc5b11c03b5c50aeedc2988697b2
aduol12 <christineaduol277@gmail.com>
2025-03-20 23:54:42 +0000

    Uncommented the Passort.js

--- real-estate-project/backend/services/authService.js
+++ real-estate-project/backend/services/authService.js
@@ -1,13 +1,54 @@
-const User = require('../models/User');
+// const User = require('../models/User');
 
+// const findOrCreateUser = async (googleId, email, name) => {
+//     let user = await User.findByGoogleId(googleId);
+//     if (!user) {
+//         user = await User.create(googleId, email, name);
+//     }
+//     return user;
+// };
+
+// module.exports = {
+//     findOrCreateUser,
+// };
+
+const pool = require('../config/db');
+
+/**
+ * Find a user by Google ID
+ */
+const findByGoogleId = async (googleId) => {
+    const result = await pool.query(
+        'SELECT * FROM users WHERE google_id = $1',
+        [googleId]
+    );
+    return result.rows[0] || null;
+};
+
+/**
+ * Create a new user
+ */
+const createUser = async (googleId, email, name) => {
+    const result = await pool.query(
+        'INSERT INTO users (google_id, email, name) VALUES ($1, $2, $3) RETURNING *',
+        [googleId, email, name]
+    );
+    return result.rows[0];
+};
+
+/**
+ * Find or create a user
+ */
 const findOrCreateUser = async (googleId, email, name) => {
-    let user = await User.findByGoogleId(googleId);
+    let user = await findByGoogleId(googleId);
     if (!user) {
-        user = await User.create(googleId, email, name);
+        user = await createUser(googleId, email, name);
     }
     return user;
 };
 
 module.exports = {
+    findByGoogleId,
+    createUser,
     findOrCreateUser,
 };
