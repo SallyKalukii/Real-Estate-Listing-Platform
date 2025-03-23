a4e8e73fd4209c6b510fc09a0d0864bd978e6143
nuerteyackam <joel.ackam@ashesi.edu.gh>
2025-03-23 21:55:30 +0000

    some changes

--- real-estate-project/backend/models/User.js
+++ real-estate-project/backend/models/User.js
@@ -1,32 +1,30 @@
-const pool = require('../config/db');
+const pool = require("../config/db");
 
 const User = {
-    // Find a user by Google ID
-    findByGoogleId: async (googleId) => {
-        const query = 'SELECT * FROM users WHERE google_id = $1';
-        const values = [googleId];
-        const { rows } = await pool.query(query, values);
-        return rows[0];
-    },
+  // Find a user by Google ID
+  findByGoogleId: async (googleId) => {
+    const query = "SELECT * FROM users WHERE google_id = $1";
+    const values = [googleId];
+    const { rows } = await pool.query(query, values);
+    return rows[0];
+  },
 
-    // Create a new user
-    create: async (googleId, email, name) => {
-        const query = `
-            INSERT INTO users (google_id, email, name)
-            VALUES ($1, $2, $3)
-            RETURNING *`;
-        const values = [googleId, email, name];
-        const { rows } = await pool.query(query, values);
-        return rows[0];
-    },
+  // Create a new user
+  create: async (googleId, email, name, profile_pic) => {
+    const query = `
+           INSERT INTO users (google_id, email, name, profile_pic, is_google_user) VALUES ($1, $2, $3, $4, $5) RETURNING *`;
+    const values = [googleId, email, name, profile_pic, true];
+    const { rows } = await pool.query(query, values);
+    return rows[0];
+  },
 
-    // Find a user by ID
-    findById: async (id) => {
-        const query = 'SELECT * FROM users WHERE id = $1';
-        const values = [id];
-        const { rows } = await pool.query(query, values);
-        return rows[0];
-    },
+  // Find a user by ID
+  findById: async (id) => {
+    const query = "SELECT * FROM users WHERE id = $1";
+    const values = [id];
+    const { rows } = await pool.query(query, values);
+    return rows[0];
+  },
 };
 
 module.exports = User;
